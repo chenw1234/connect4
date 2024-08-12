74a2a550e5e59acf9538c814f6c2f7ca8a2b9d6f
William <chenw123@DESKTOP-PHMQ0DM>
2024-08-12 15:49:17 -0700

    with comments

--- connect4.js
+++ connect4.js
@@ -31,6 +31,7 @@ function makeHtmlBoard() {
   const top = document.createElement("tr");
   top.setAttribute("id", "column-top");
   top.addEventListener("click", handleClick);
+  //Here we've created the BEGINNING row of the top clickable row of the board. It will change colors when hovered over.
 
   for (let x = 0; x < WIDTH; x++) {
     const headCell = document.createElement("td");
@@ -38,17 +39,20 @@ function makeHtmlBoard() {
     top.append(headCell);
   }
   board.append(top);
+  //Here we are adding cells horizontally from the beginning top row under the const "top" and labeling each cell with an x value until it reaches the maximum width of 7
 
   // TODO: add comment for this code
   for (let y = 0; y < HEIGHT; y++) {
     const row = document.createElement("tr");
-
+    //Above, we are building more empty vertical rows below the "top" clickable row until we reach the maximum height of 6
     for (let x = 0; x < WIDTH; x++) {
       const cell = document.createElement("td");
       cell.setAttribute("id", `${y}-${x}`);
       row.append(cell);
     }
+    //Above, we are building horizontal cells on each newly made vertical row. The cells will be built until they reach the maximum width of 7 and each cell is being labeled an id of its (y, x) coordinate using the setAttribute. Each cell is then appended to the empty row horizontally until width is reached.
     board.append(row);
+    //Each row that has been fully filled with horizontal cells is then added to the main board array that was initially empty at the start. We are just constructing the board below the clickable top row right now.
   }
 }
 
@@ -57,11 +61,15 @@ function makeHtmlBoard() {
 function findSpotForCol(x) {
   // TODO: write the real version of this, rather than always returning 0
   for (let y = HEIGHT - 1; y >= 0; y--) {
+    //We start at y = HEIGHT -1; since we are finding the x spot BELOW the top clickable row. The loop then loops downward through the entire board until the last row is reached. 
+    //This prevents pieces from filling the top clickable row
     if (!board[y][x]) {
+      //Here, 
       return y;
     }
   }
   return null;
+  //Does not return a y value for the piece to enter if the next slot to be filled is inside the top clickable row.
 }
 
 /** placeInTable: update DOM to place piece into HTML table of board */
@@ -72,6 +80,7 @@ function placeInTable(y, x) {
   piece.classList.add('piece');
   piece.classList.add(`p${currPlayer}`);
   piece.style.top = -50 * (y + 2);
+  //??? above
   const spot = document.getElementById(`${y}-${x}`);
   spot.append(piece);
 }
@@ -88,11 +97,12 @@ function endGame(msg) {
 function handleClick(evt) {
   // get x from ID of clicked cell
   const x = +evt.target.id;
-
+  //ask about +evt
   // get next spot in column (if none, ignore click)
   const y = findSpotForCol(x);
   if (y === null) {
     return;
+    //return nothing if the column reaches the top
   }
 
   // place piece in board and add to HTML table
